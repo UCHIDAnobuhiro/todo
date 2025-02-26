9748db19e5d7c2c98bef3d0f920d131282b48df5
Ouchinitsu <ouchinitsu@gmail.com>
2025-02-26 20:37:55 +0900

    ID&Passwordの検証機能完成
    2/26 20:37
    Ou

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,9 +11,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
 
 @Controller
 public class UserController {
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@RequestMapping("/")
 	public String showLogin() {
 		return "login";
@@ -18,16 +26,27 @@ public class UserController {
 
 	@GetMapping("/")
 	public String setUser(Model model) {
-		model.addAttribute("user", new User());
+		User user = new User();
+		model.addAttribute("user", user);
 		return "login";
-
 	}
 
 	@PostMapping("/show")
 	public String requestMethodName(@ModelAttribute User user, Model model) {
-		//データベースでuserをチェックし、あるなら/showを表示する、ないならrootに戻る
+		//データベースでuserをチェックし、あるなら/showを表示する、ないならloginに戻る
+		List<User> users = userRepository.findAll();
 		model.addAttribute(user);
-		return "/";
+
+		for (int i = 0; i < users.size(); i++) {
+			if (users.get(i).getEmail().equals(user.getEmail())
+					&& users.get(i).getPassword().equals(user.getPassword())) {
+				System.out.println("ID:" + users.get(i).getEmail() + "PW:" + users.get(i).getPassword());
+				return "show";
+			}
+		}
+		System.out.println(user.getEmail());
+		return "login";
+
 	}
 
 }
