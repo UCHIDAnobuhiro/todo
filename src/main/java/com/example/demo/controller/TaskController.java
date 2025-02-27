9f5319953a4d3c89d2729de06ea82df40e51b39f
UCHIDAnobuhiro <92204227+UCHIDAnobuhiro@users.noreply.github.com>
2025-02-27 13:59:50 +0900

    fix class名の変更

--- src/main/java/com/example/demo/controller/TaskController.java
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -4,11 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class ListController {
+public class TaskController {
 
 	@GetMapping("/todo/create")
-	public String creataPage() {
+	public String createPage() {
 		return "todo/create-todo";
 	}
 
+	@GetMapping("/todo/database")
+	public String databasePage() {
+		return "todo/database";
+	}
 }
